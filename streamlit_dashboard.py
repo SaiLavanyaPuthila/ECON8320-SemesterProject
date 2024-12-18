32bc0e890932b5f82ce1ecbf3ff87625470b8447
SaiLavanyaPuthila <sputhila@unomaha.edu>
2024-12-18 16:18:03 +0530

    Fixed import errors

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -3,12 +3,12 @@ import pandas as pd
 import warnings
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from plots.plot_2 import plot_1
-from plots.plot_1 import plot_2
-from plots.plot_3 import plot_5
-from plots.plot_4 import plot_6
-from plots.plot_5 import plot_7
-from plots.plot_6 import plot_10
+from plots.plot_1 import plot_1
+from plots.plot_2 import plot_2
+from plots.plot_3 import plot_3
+from plots.plot_4 import plot_4
+from plots.plot_5 import plot_5
+from plots.plot_6 import plot_6
 
 
 warnings.filterwarnings("ignore")
@@ -20,25 +20,14 @@ st.markdown(
     "<style>div.block-container{padding-top:2rem;}</style>", unsafe_allow_html=True
 )
 
+plot_1()
 
 plot_2()
 
-plot_1()
+plot_3()
 
-
-
-# plot_3()
-
-# plot_4()
+plot_4()
 
 plot_5()
 
 plot_6()
-
-plot_7()
-
-# plot_8()
-
-# plot_9()
-
-plot_10()
